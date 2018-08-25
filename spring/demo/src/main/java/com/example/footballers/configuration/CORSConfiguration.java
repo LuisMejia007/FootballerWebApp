c1a5116318a2e696f1139b3cb86226e6ed0eeaf9
LuisMejia007 <paranoidandroidz180@gmail.com>
2018-08-25 00:46:52 -0700

    Added updated CORS configuration inside spring

--- spring/demo/src/main/java/com/example/footballers/configuration/CORSConfiguration.java
+++ spring/demo/src/main/java/com/example/footballers/configuration/CORSConfiguration.java
@@ -18,7 +18,13 @@ public class CORSConfiguration{
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("*").allowedOrigins("http://localhost:1200").allowedMethods("*");
+
+                /*
+                *   /addFootballer
+                *
+                * */
+                registry.addMapping("*").allowedOrigins("http://localhost:1200").allowedMethods("GET, POST");
+
             }
         };
     }
